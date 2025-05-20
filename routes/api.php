33d094099f5d49196594a40ee084b597478b0451
Kristof Grenson <kristof@nizer.be>
2025-05-20 14:21:05 +0200

    sanctom token based (too) minimal auth

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,48 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('products', ProductController::class)->only(['index', 'store', 'show', 'destroy', 'update']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('products', ProductController::class)->only(['index', 'store', 'show', 'destroy', 'update']);
+    Route::apiResource('categories', CategoryController::class)->only(['index']);
+});
 
-Route::apiResource('categories', CategoryController::class)->only(['index']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::post('/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    return response()->json(['token' => $token]);
+});
