130668d243836aeb597368faab6ca8243f86f264
Kristof Grenson <kristof@nizer.be>
2025-05-20 14:21:34 +0200

    voorbeeldje post in laravel als front-end

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+// Route::post('/contact', function () {
+//     return 'post gelukt';
+// });
+
+// create a route for the contact form to show the form on GET
+Route::get('/contact', [ContactController::class, 'create']);
+Route::post('/contact', [ContactController::class, 'store']);
+
 
 Route::middleware([
     'auth:sanctum',
